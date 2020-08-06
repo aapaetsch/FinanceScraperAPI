9dcd92ddd4919a3b2423941978553787f8c302bf
Aidan Paetsch <aapaetsch@gmail.com>
2020-08-06 03:43:49 -0600

    separating fetch and update functions

--- stonksAPI.py
+++ stonksAPI.py
@@ -1,11 +1,14 @@
 #Basic flask + mongodb modified from https://spapas.github.io/2014/06/30/rest-flask-mongodb-heroku/
 import os
 import time
+import random
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask import make_response
-from yahooScraper import yahooFinanceScraper as yfs
+from yahooScraper import yahooScraper as yfs
 from bson.json_util import dumps
+from proxyScraper import runScraper as getProxies
+
 
 MONGO_URL = os.environ.get('MONGO_URL')
 if not MONGO_URL:
@@ -15,6 +18,8 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['MONGO_URI'] = MONGO_URL
 mongo = PyMongo(app)
+
+
 
 # print(mongo.db.getCollection('currentTickerValues'))
 
@@ -27,7 +32,83 @@ def home():
 		<p>This api fetches the current stock data for a users portfolio</p>
 	'''
 
-@app.route('/stonksAPI/v1/current/single', methods=['GET'])
+@app.route('/stonksAPI/v1/current/single/exists', methods=['GET'])
+#checker for tickers if they exist.... 
+def checkTickerExists():
+	pass 
+
+@app.route('/stonksAPI/v1/update', methods=['GET'])
+def updateValues():
+	print('hello')
+	#First we update the ticker values
+	cursor = mongo.db.currentTickerValues
+	stocks = cursor.find({}, {'_id': 0})
+	print(stocks)
+	scraper = yfs()
+	for s in stocks:
+		print(s)
+		query = {'ticker': s['ticker']}
+
+		if (time.time() - s['lastUpdate']) > 28800:
+			time.sleep(random.uniform(0,3))
+			scraper.runValueScraper(s['ticker'])
+			newData = scraper.getCurrentValueData()[0]
+			scraper.resetCurrent()
+
+			if newData != []:
+				print(newData)
+				if newData['price'] != 0.00:
+					cursor.update_one(query, {'$set':newData})
+
+
+	#Second we update the indicies
+	cursor2 = mongo.db.indicies
+	inds = cursor2.find_one()
+	print(inds)
+	if inds == None:
+		x = scraper.runTableScraper('indicies')
+		print(x, scraper.getIndicies())
+		if x:
+			newData = scraper.getIndicies()
+			print(newData)
+			cursor2.insert_many(newData)
+	else:
+		if (time.time() - inds['lastUpdate']) > 28800:
+			if scraper.runTableScraper('indicies'):
+				newData = scraper.getIndicies()
+				print(newData)
+				if newData != []:
+					for d in newData:
+						cursor2.update_one({'ticker': d['ticker']}, {'$set':d})
+						print(d)
+
+
+	#Third we update the currencies
+	cursor3 = mongo.db.currencies
+	munny = cursor3.find_one()
+
+	if munny == None:
+		if scraper.runTableScraper('currencies'):
+			newData = scraper.getCurrencies()
+			print(newData)
+			cursor3.insert_many(newData)
+	
+	else:
+		if (time.time() - munny['lastUpdate']) > 28800:
+			newData = scraper.getCurrencies()
+			if newData != []:
+				for d in newData:
+					cursor3.update_one({'ticker': d['ticker']}, {'$set':d})	
+
+	return '''
+	<h1>Stonks API</h1>
+	<p>The API just updated!</p>
+	'''
+	
+
+
+#add detection for a bad return value
+@app.route('/stonksAPI/v1/current/single', methods=['GET', 'POST'])
 def singleLookup():
 
 	if 'ticker' in request.args:
@@ -44,11 +125,12 @@ def singleLookup():
 	#Fetch new data every 8 hrs
 	if stock == None:
 		try:
-			scraper.runCurrentValueScraper(ticker)
+			scraper.runValueScraper(ticker)
 			newData = scraper.getCurrentValueData()[0]
-			print('\nNewData:\t',newData)
-			cursor.insert_one(newData)
-			stock = cursor.find_one(query, {"_id": 0})
+			if newDat['price'] != 0.00:
+				print('\nNewData:\t',newData)
+				cursor.insert_one(newData)
+				stock = cursor.find_one(query, {"_id": 0})
 
 		except:
 			response = jsonify(stock)
@@ -56,14 +138,32 @@ def singleLookup():
 			return response
 	
 	elif (time.time() - stock['lastUpdate']) > 28800:
-		scraper.runCurrentValueScraper(ticker)
+		scraper.runValueScraper(ticker)
 		newData = scraper.getCurrentValueData()[0]
 		print('\nNewData Time:\t',newData)
-		cursor.update_one(query, {"$set": newData})
-		stock = cursor.find_one(query, {"_id": 0})
+		if newData['price'] != 0.00:
+			cursor.update_one(query, {"$set": newData})
+			stock = cursor.find_one(query, {"_id": 0})
 	
 	response = jsonify(stock)
 	response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+	print(response)
 	return response
+
+
+#This method will look up the current values of indicies
+@app.route('/stonksAPI/v1/indicies/current', methods=['GET', 'POST'])
+def indiciesLookup():
+	cursor = mongo.db.indicies
+	inds = cursor.find()
+	return jsonify(inds)
+
+
+#This method will look up the current values of currencies
+@app.route('/stonksAPI/v1/currencies/current', methods=['GET', 'POST'])
+def currenciesLookup():
+	cursor = mongo.db.currencies
+	monies = cursor.fin()
+	return jsonify(monies)
 
 app.run()
--- yahooScraper.py
+++ yahooScraper.py
@@ -6,110 +6,324 @@ from datetime import datetime
 from datetime import timedelta
 import time
 import random
+# from collections import OrderedDict
+# from itertools import cycle
 
-class yahooFinanceScraper:
-	def __init__(self):
-		self.__currentValueData = []
-	
-	def runCurrentValueScraper(self, ticker):
-		print('\n\n\n\n\nscraper running\n\n\n\n\n\n\n')
-		ticker = ticker.upper()
-		url = 'https://ca.finance.yahoo.com/quote/{}?p={}'.format(ticker, ticker)
-		session = requests_html.HTMLSession()
-		r = session.get(url)
-		content = bs(r.content, 'lxml')
 
-		try:
-			ids = [9, 33, 32]
-			# random.shuffle(ids)
-			# make the scraper more robust
-			data = content.find_all('span', {'data-reactid': ids})
+# proxies = []
+# with open('proxies.txt', 'r') as f:
+#     for line in f:
+#         proxies.append(line)
+# print(proxies)
+# headers_list = [
+#     # Firefox 77 Mac
+#      {
+#         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#         "Accept-Language": "en-US,en;q=0.5",
+#         "Referer": "https://www.google.com/",
+#         "DNT": "1",
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1"
+#     },
+#     # Firefox 77 Windows
+#     {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#         "Accept-Language": "en-US,en;q=0.5",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Referer": "https://www.google.com/",
+#         "DNT": "1",
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1"
+#     },
+#     # Chrome 83 Mac
+#     {
+#         "Connection": "keep-alive",
+#         "DNT": "1",
+#         "Upgrade-Insecure-Requests": "1",
+#         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#         "Sec-Fetch-Site": "none",
+#         "Sec-Fetch-Mode": "navigate",
+#         "Sec-Fetch-Dest": "document",
+#         "Referer": "https://www.google.com/",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+#     },
+#     # Chrome 83 Windows 
+#     {
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1",
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#         "Sec-Fetch-Site": "same-origin",
+#         "Sec-Fetch-Mode": "navigate",
+#         "Sec-Fetch-User": "?1",
+#         "Sec-Fetch-Dest": "document",
+#         "Referer": "https://www.google.com/",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Accept-Language": "en-US,en;q=0.9"
+#     },
+#     {
+#         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#         "Accept-Language": "en-US,en;q=0.5",
+#         "Referer": "https://www.tutorialspoint.com/flask/flask_http_methods.htm",
+#         "DNT": "1",
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1"
+#     },
+#     # Firefox 77 Windows
+#     {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#         "Accept-Language": "en-US,en;q=0.5",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Referer": "https://www.google.com/search?q=1+day+in+seconds&rlz=1C1CHBF_enCA867CA867&oq=1+day+in+seconds&aqs=chrome..69i57j6.2720j0j4&sourceid=chrome&ie=UTF-8",
+#         "DNT": "1",
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1"
+#     },
+#     # Chrome 83 Mac
+#     {
+#         "Connection": "keep-alive",
+#         "DNT": "1",
+#         "Upgrade-Insecure-Requests": "1",
+#         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#         "Sec-Fetch-Site": "none",
+#         "Sec-Fetch-Mode": "navigate",
+#         "Sec-Fetch-Dest": "document",
+#         "Referer": "https://blog.upperlinecode.com/flask-and-firebase-and-pyrebase-oh-my-f30548d68ea9",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+#     },
+#     # Chrome 83 Windows 
+#     {
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1",
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#         "Sec-Fetch-Site": "same-origin",
+#         "Sec-Fetch-Mode": "navigate",
+#         "Sec-Fetch-User": "?1",
+#         "Sec-Fetch-Dest": "document",
+#         "Referer": "https://www.google.com/",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Accept-Language": "en-US,en;q=0.9"
+#     }
+#     ,
+#     # Chrome 83 Windows 
+#     {
+#         "Connection": "keep-alive",
+#         "Upgrade-Insecure-Requests": "1",
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#         "Sec-Fetch-Site": "same-origin",
+#         "Sec-Fetch-Mode": "navigate",
+#         "Sec-Fetch-User": "?1",
+#         "Sec-Fetch-Dest": "document",
+#         "Referer": "https://www.google.com/",
+#         "Accept-Encoding": "gzip, deflate, br",
+#         "Accept-Language": "en-US,en;q=0.9"
+#     }
+# ]
 
-		except:
-			data = []
+# ordered_headers_list = []
+# for headers in headers_list:
+#     h = OrderedDict()
+#     for header, value in headers.items():
+#         h[header]=value
+#     ordered_headers_list.append(h)
+# random.shuffle(proxies)
+# proxy_pool = cycle(proxies)
 
-		if data != []:
 
-			location = data[1]
-			price = data[2]
-			change = data[3]
-			
-			location, currency = self.__stripLocation(location)
-			price = self.__stripPrice(price)
-			change, changePercent = self.__stripChange(change)
-		else:
-			price = change = changePercent = 0.00
-			location = currency = 'Unknown'
+class yahooScraper:
+    def __init__(self):
+        self.__currentValueData = []
+        self.__indiciesData = []
+        self.header = {
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
 
-		try:
-			stockTitle = content.find_all('h1')
-			title = self.__stripTitle(stockTitle[0])
-		except:
-			#redundant....
-			title = 'Unknown'
+    def runValueScraper(self, ticker):
+        #This method scrapes the current quote data for a ticker
+        print('\n\n\n\n\nscraper running\n\n\n\n\n\n\n')
+        time.sleep(random.uniform(0,6))
+        # p = next(proxy_pool).replace('\n','')
+        ticker = ticker.upper()
+        url = 'https://ca.finance.yahoo.com/quote/{}?p={}'.format(ticker, ticker)
+        session = requests_html.HTMLSession()
+        r = session.get(url, headers=self.header)
+        content = bs(r.content, 'lxml')
+        
+        # try:
+        #     # r = session.get(url,proxies={'https': p, 'http': p}, headers=random.choice(headers_list))
 
-		self.__currentValueData.append({'ticker': ticker, 'price': price, 'currency': currency, 'exchange': location,
-		'change': change, 'changePercent': changePercent, 'title': title, 'lastUpdate': time.time()})
-	
-	def scrapeMultipleTickers(self, tickers):
-		for ticker in tickers:
-			self.runCurrentValueScraper(ticker)
-			time.sleep(random.uniform(2))
+        # except:
+        #     self.runScraper(ticker)
+        #     print('Error with proxie:', p)
+        #     proxies.remove(p)
 
-	def __stripLocation(self, locStr):
-		try:
-			locList = self.__getCleanList(locStr)
-			locList = locList[1].split(' ')
-			location = locList[0]
-			currency = locList[-1]
+        try:
+            ids = [9, 33, 32]
+            # random.shuffle(ids)
+            # make the scraper more robust
+            data = content.find_all('span', {'data-reactid': ids})
 
-		except:
-			location = 'Unknown'
-			currency = 'Unknown'
+        except:
+            data = []
 
-		return location, currency
+        if data != []:
 
-	def __stripPrice(self, priceStr):
-		try: 
-			priceList = self.__getCleanList(priceStr)
-			price = float(priceList[-1].replace(' ', '').replace(',', ''))
-			
-		except:
-			price = 0.00
+            location = data[1]
+            price = data[2]
+            change = data[3]
+            
+            location, currency = self.__stripLocation(location)
+            price = self.__stripPrice(price)
+            change, changePercent = self.__stripChange(change)
+        else:
+            price = change = changePercent = 0.00
+            location = currency = 'Unknown'
 
-		return price
+        try:
+            stockTitle = content.find_all('h1')
+            title = self.__stripTitle(stockTitle[0])
+        except:
+            #redundant....
+            title = 'Unknown'
 
-	def __stripChange(self, changeStr):
-		try:
-			changeList = self.__getCleanList(changeStr)
-			changeList = changeList[-1].split(' ')
-			change = float(changeList[0].replace(' ', '').replace(',', ''))
-			changePercent = float(changeList[-1].replace(' ', '').strip('()%').replace(',',''))
-		except:
-			change = 0.00
-			changePercent = 0.00
 
-		return change, changePercent
+        self.__currentValueData.append({'ticker': ticker, 'price': price, 'currency': currency, 'exchange': location,
+        'change': change, 'changePercent': changePercent, 'title': title, 'lastUpdate': time.time()})
+    
+    def scrapeMultipleTickers(self, tickers):
+        for ticker in tickers:
+            self.runCurrentValueScraper(ticker)
+            time.sleep(random.uniform(0,4))
 
-	def __stripTitle(self, titleStr):
-		try:
-			titleList = str(titleStr).strip('</h1>').split('>')
-			title = titleList[-1]
+    def __stripLocation(self, locStr):
+        try:
+            locList = self.__getCleanList(locStr)
+            locList = locList[1].split(' ')
+            location = locList[0]
+            currency = locList[-1]
 
-		except:
-			title = 'Unknown'
+        except:
+            location = 'Unknown'
+            currency = 'Unknown'
 
-		return title
+        return location, currency
 
-	def __getCleanList(self, string):
-		return str(string).strip('</span>').split('>')
+    def __stripPrice(self, priceStr):
+        try: 
+            priceList = self.__getCleanList(priceStr)
+            price = float(priceList[-1].replace(' ', '').replace(',', ''))
+            
+        except:
+            price = 0.00
 
-	def getCurrentValueData(self):
-		return self.__currentValueData
+        return price
+
+    def __stripChange(self, changeStr):
+        try:
+            changeList = self.__getCleanList(changeStr)
+            changeList = changeList[-1].split(' ')
+            change = float(changeList[0].replace(' ', '').replace(',', ''))
+            changePercent = float(changeList[-1].replace(' ', '').strip('()%').replace(',',''))
+        except:
+            change = 0.00
+            changePercent = 0.00
+
+        return change, changePercent
+
+    def __stripTitle(self, titleStr):
+        try:
+            titleList = str(titleStr).strip('</h1>').split('>')
+            title = titleList[-1]
+
+        except:
+            title = 'Unknown'
+
+        return title
+
+    def __getCleanList(self, string):
+        return str(string).strip('</span>').split('>')
+
+    def getCurrentValueData(self):
+        return self.__currentValueData
+
+    def runTableScraper(self, page):
+        #This method can be used to scrape currencies or indicies
+        if page != 'currencies' or page != 'indicies':
+            return False
+        print('\n\n\n\n\nscraper running\n\n\n\n\n\n\n')
+        time.sleep(random.uniform(0,6))
+        url = 'https://ca.finance.yahoo.com/'+ page
+        session = requests_html.HTMLSession()
+        r = session.get(url, headers=self.header)
+        content = bs(r.content, 'lxml')
+        
+        try:
+            print(content)
+            data = content.find_all('tr')
+            print(data)
+        except:
+            return False
+
+
+        for d in data:
+            content = bs(d, 'lxml')
+            items = content.find_all('td')
+            ind = {}
+            colNames = ['ticker', 'name', 'value', 'change', 'changePercent', 'volume']
+            i = 0 
+            for col in items:
+                value = col.contents[0].string
+                if value != None:
+                    print(value)
+                    ind[colNames[i]] = value
+                i += 1
+            
+            if ind.get('ticker') != None:
+                ind['lastUpdate'] = time.time()
+                if page == 'indicies':
+                    self.__indiciesData.append(ind)
+                elif page == 'currencies':
+                    self.__currencies.append(ind)
+
+        return True
+
+    def getIndicies(self):
+        return self.__indiciesData
+
+    def getCurrencies(self):
+        return self.__currencies
+
+    def resetCurrencies(self):
+        self.__currencies = []
+
+    def resetIndicies(self):
+        self.__indiciesData = []
+
+    def resetCurrent(self):
+        self.__currentValueData = []
+
 
 
 
 if __name__ == '__main__':
-	scrapy = yahooFinanceScraper('vgg.to')
-	scrapy.runScraper()
-	print(scrapy.getAll())
+    scrapy = yahooIndicesScraper()
+    scrapy.runScraper()
